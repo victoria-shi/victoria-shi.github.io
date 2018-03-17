15f5ba634fec04edf059d2dd481584b9148eb172
Victoria Shi <victoria.shi@healthtap.com>
2018-03-16 23:31:23 -0700

    resume updates

--- src/javascripts/components/resume/roleSection.jsx
+++ src/javascripts/components/resume/roleSection.jsx
@@ -6,16 +6,11 @@ function RoleSection(props) {
     const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Oct', 'Nov', 'Dec'];
     return `${months[date.getMonth()]} ${date.getFullYear()}`
   }
-  const description = props.description.map((item, index) => {
-    return (
-      <li key={index}>{item}</li>
-    );
-  });
   return (
     <div className='roleSection'>
       <div className="title">{`${props.title}, ${props.company}`}</div>
       <div className="dates">{`${renderDate(props.dates[0])} - ${renderDate(props.dates[1])}`}</div>
-      <ul className="description">{description}</ul>
+      <div className="content">{props.children}</div>
     </div>
   );
 }
@@ -23,8 +18,7 @@ function RoleSection(props) {
 RoleSection.propTypes = {
   title: PropTypes.string.isRequired,
   company: PropTypes.string.isRequired,
-  dates: PropTypes.arrayOf(PropTypes.object).isRequired,
-  description: PropTypes.arrayOf(PropTypes.string).isRequired
+  dates: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default RoleSection;
--- src/javascripts/components/resumePage.jsx
+++ src/javascripts/components/resumePage.jsx
@@ -16,57 +16,125 @@ class ResumePage extends React.Component {
           company: 'HealthTap',
           dates: [new Date(2016, 6), new Date()],
           description: [
-            'Tech lead - interactive care plans',
-            'Web frontend lead - globalization',
-            'Web frontend lead - enterprise partnerships (Bupa Healthcare Group, Discovery Limited) ',
+            { text: 'Build and maintain the HealthTap web app.' },
+            {
+              text: 'Technical lead, Interactive Care Plans product',
+              class: 'bold',
+              description: [
+                { text: 'Own and drive the entire engineering effort.' },
+                { text: 'Coordinate 15+ engineers to successfully deliver the product on time.' }
+              ]
+            },
+            {
+              text: 'Frontend team lead, globalization and internationalization',
+              class: 'bold',
+              description: [
+                { text: 'Enable translation of site into Mandarin Chinese, Spanish, and other languages.' },
+                { text: 'Enable localization of dates and times, currencies, and other locale-sensitive information.' },
+                { text: 'Enforce globalization best practices among web team.' }
+              ]
+            },
+            {
+              text: 'Frontend team lead, enterprise projects',
+              class: 'bold',
+              description: [
+                { text: 'Interface with enterprise healthcare providers (Bupa Healthcare Group, Discovery Limited) and provide software solutions for thousands of their patients, doctors, and nurses. ' }
+              ]
+            }
           ]
         },
         {
           title: 'UI/UX Designer, MyGreenCar Project',
           company: 'Berkeley Lab',
           dates: [new Date(2015, 9), new Date(2016, 5)],
-          description: []
+          description: [
+            { text: 'Design and develop the MyGreenCar web app.' },
+            { text: 'Create design mockups and final designs, images and assets for the project.' },
+            { text: 'Build front end of the web app.' }
+          ]
         },
         {
           title: 'UI/UX Designer',
           company: 'Twindom',
           dates: [new Date(2015, 5), new Date(2015, 7)],
-          description: []
+          description: [
+            { text: 'Design and implement new UI/UX for both web and mobile.' },
+            { text: 'Develop iOS app for users to view their Twindom 3D scans.' },
+            { text: 'Wordpress / Magento / Swift' }
+          ]
+        },
+        {
+          title: 'Teaching Assistant, CS10 Course',
+          company: 'UC Berkeley',
+          dates: [new Date(2014, 0), new Date(2016, 5)],
+          description: [
+            { text: 'Lead biweekly labs and weekly discussion sections.' },
+            { text: 'Support students at weekly office hours.' },
+            { text: 'Draft and grade class homework, lab assignments, quizzes, projects, and exams.' }
+          ]
         }
       ]
     }
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  renderContent(data, index) {
+    const content = [];
+    if (data.description && data.description.length > 0) {
+      data.description.forEach((d, index) => {
+        content.push(this.renderContent(d, index));
+      });
+    } else {
+      return (<li key={index}>{data.text}</li>);
+    }
+    return (
+      <div>
+        <li className={data.class || ''}>{data.text}</li>
+        <li style={{ listStyleType: 'none' }}>
+          <ul>{content}</ul>
+        </li>
+      </div>
+    );
   }
 
   render() {
+    const self = this;
     const roleSections = this.model.roles.map((role, index) => {
+      const roleContent = [];
+      role.description.forEach((d, index) => { roleContent.push(self.renderContent(d, index)); });
       return (
-        <RoleSection
-          key={index} title={role.title} company={role.company}
-          dates={role.dates} description={role.description}
-        />
+        <RoleSection key={index} title={role.title} company={role.company} dates={role.dates}>
+          <ul>{roleContent}</ul>
+        </RoleSection>
       );
     })
     return (
       <PageLayout>
         <div className="resume">
-          <ResumeSection index={1} title='Education'>
+          <ResumeSection index={1} title='Education' class="education">
             <p>University of California, Berkeley</p>
             <p>BA Computer Science</p>
             <p>May 2016</p>
           </ResumeSection>
-          <ResumeSection index={2} title='Stack'>
-            <p>Javascript / jQuery / React></p>
-            <p>HTML5 / CSS3 / Sass</p>
-            <p>Rails / Python</p>
+          <ResumeSection index={2} title='Stack' class="stack">
+            <span>Javascript</span>
+            <span>jQuery</span>
+            <span>React</span>
+            <span>HTML5</span>
+            <span>CSS3</span>
+            <span>Sass</span>
+            <span>Rails</span>
+            <span>Python</span>
           </ResumeSection>
-          <ResumeSection index={3} title='Software'>
-            <p>Adobe Creative Suite / Photoshop / Illustrator</p>
-            <p>Balsamiq</p>
+          <ResumeSection index={3} title='Software' class="software">
+            <span>Adobe Creative Suite</span>
+            <span>Photoshop</span>
+            <span>Illustrator</span>
           </ResumeSection>
           <ResumeSection index={4} title='Experience' class='experience'>
             {roleSections}
           </ResumeSection>
-          <ResumeSection index={5} title='Contact'>
+          <ResumeSection index={5} title='Contact' class="contact">
             <a href="mailto:victoria.shi@berkeley.edu">victoria.shi@berkeley.edu</a>
             <a href="tel:1-626-232-5813">(626) 232-5813</a>
             <p>1375 Montecito Ave Apt. 2</p>
